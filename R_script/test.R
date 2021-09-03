6564a3edea10281062754101f1962718b3861e15
Victoria Caudill <viccaud@gmail.com>
2021-09-02 18:53:20 -0700

    testing git 3rd time

--- R_script/test.R
+++ R_script/test.R
@@ -7,5 +7,6 @@ place<-"data/size/"
 split_df = make_dichotomy(file_info, place)
 #adding a line for git
 #second test for git
+# a third test for good measure
 file <- data.frame(split_df[1])
 dichotomy<-data.frame(split_df[2])
