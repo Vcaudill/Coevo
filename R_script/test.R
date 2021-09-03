e41bf08122f11627ec171bf596f36abecb098d0e
Victoria Caudill <viccaud@gmail.com>
2021-09-02 18:48:18 -0700

    testing git

--- R_script/test.R
+++ R_script/test.R
@@ -5,6 +5,6 @@ file_info<- list.files("data/size", pattern="exp_K") #new files that i turned of
 place<-"data/size/"
 
 split_df = make_dichotomy(file_info, place)
-
+#adding a line for git
 file <- data.frame(split_df[1])
 dichotomy<-data.frame(split_df[2])
